0cee87a26686c288c01d87f446b142d346b9b87c
priyanshirabadiya <139095730+priyanshirabadiya@users.noreply.github.com>
2024-03-19 20:39:22 +0530

    19/3/24

---
+++ 4_number_methods/javascript.js
@@ -0,0 +1,9 @@
+let x = 120;
+// x.toString();
+// console.log((123).toString());
+// console.log(123);
+// (123 + 20).toString();
+
+// console.log((123 + 20).toString());
+
+document.getElementById("demo").innerHTML = x;
