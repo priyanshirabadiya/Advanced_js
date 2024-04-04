c67b42cba3ae3fe8e69a1b3ec638a7248864e7c0
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-04-04 19:58:41 +0530

    4/4/24

---
+++ 10_Arrays/javascript.js
@@ -0,0 +1,20 @@
+let num = [3 , 54 , 5 , 8 , 6 ];
+
+// for(i = 0 ; i < num.length ; i++)
+// {
+//     console.log(num[i]);
+// }
+
+
+
+
+
+// function myFun(item) {
+//     console.log(item * 2);
+// }
+
+// num.forEach(myFun);
+
+const string = "Hello";
+let Arr = Array.from(string);
+console.log(Arr);
